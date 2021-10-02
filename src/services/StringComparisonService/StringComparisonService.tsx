5fa5d452e0b221a4991103a3035a652133893650
codedghost2 <codedghost2@gmail.com>
2021-10-02 23:27:49 +0100

    Fixing string comparison method

--- src/services/StringComparisonService/StringComparisonService.tsx
+++ src/services/StringComparisonService/StringComparisonService.tsx
@@ -3,5 +3,5 @@ export function Equals(a: string, b: string, insensitive: boolean = true) {
 
     if (a == null || b == null) return false;
     
-    return a.localeCompare(b, undefined, { sensitivity: insensitive ? 'accent' : '' })
+    return a.localeCompare(b, 'en', { sensitivity: insensitive ? 'accent' : '' }) === 0;
 }
